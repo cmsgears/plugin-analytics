be379525dd7487b2073e8177435c5f618df4ed7f
Bhagwat Chouhan <bhagwat.chouhan@gmail.com>
2017-06-02 20:23:06 +0530

    Initial setup.

---
+++ config/GoogleAnalyticsProperties.php
@@ -0,0 +1,44 @@
+<?php
+namespace cmsgears\analytics\google\config;
+
+// CMG Imports
+use cmsgears\core\common\config\CmgProperties;
+
+class GoogleAnalyticsProperties extends CmgProperties {
+
+	const CONFIG_GOOGLE_ANALYTICS	= 'google-analytics';
+
+	const PROP_GLOGAL				= 'global';
+
+	const PROP_GLOGAL_CODE			= 'global_code';
+
+	// Singleton instance
+	private static $instance;
+
+	// Constructor and Initialisation ------------------------------
+
+	/**
+	 * Return Singleton instance.
+	 */
+	public static function getInstance() {
+
+		if( !isset( self::$instance ) ) {
+
+			self::$instance	= new GoogleAnalyticsProperties();
+
+			self::$instance->init( self::CONFIG_GOOGLE_ANALYTICS );
+		}
+
+		return self::$instance;
+	}
+
+	public function isGlobal() {
+
+		return $this->properties[ self::PROP_GLOGAL ];
+	}
+
+	public function getGlobalCode() {
+
+		return $this->properties[ self::PROP_GLOGAL_CODE ];
+	}
+}
---
+++ widgets/GaTracker.php
@@ -0,0 +1,46 @@
+<?php
+namespace cmsgears\analytics\google\widgets;
+
+// Yii Imports
+use \Yii;
+use yii\helpers\Html;
+
+// CMG Imports
+use cmsgears\analytics\google\config\GoogleAnalyticsProperties;
+
+class GaTracker extends \cmsgears\core\common\base\Widget {
+
+	// Variables ---------------------------------------------------
+
+	// Public Variables --------------------
+
+	public $active;
+
+	public $code;
+
+	// Constructor and Initialisation ------------------------------
+
+	// yii\base\Object
+
+    public function init() {
+
+        parent::init();
+
+        $properties 	= GoogleAnalyticsProperties::getInstance();
+
+        $this->active	= isset( $this->active ) ? $this->active : $properties->isGlobal();
+
+        $this->code		= isset( $this->code) ? $this->code : $properties->getGlobalCode();
+    }
+
+	// Instance Methods --------------------------------------------
+
+	// yii\base\Widget
+
+	// cmsgears\core\common\base\Widget
+
+    public function renderWidget( $config = [] ) {
+
+    	return $this->render( $this->template, [ 'widget' => $this ] );
+    }
+}
